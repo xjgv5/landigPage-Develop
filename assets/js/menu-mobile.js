c797bdd02d1f03cf8323c458ee509ae0007775fe
Javier Gomez <javier.gov@outlook.es>
2024-09-27 23:01:05 -0600

    Js para menu responsivo terminado

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -2,5 +2,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let mobile_btn = document.querySelector(".navbar__mobile-btn")
     let mobile_menu = document.querySelector(".menu-mobile")
 
+    mobile_btn.addEventListener("click", ()=>{
+        let show = document.querySelector(".menu-mobile--show")
+
+        if(show){
+            mobile_menu.classList.remove("menu-mobile--show")
+        } else{
+            mobile_menu.classList.add("menu-mobile--show")
+        }
+    })
     
 })
