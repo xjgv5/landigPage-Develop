68b3412ac28c724794eb7b03290583dbb8509a8f
Javier Gomez <javier.gov@outlook.es>
2024-09-30 23:30:24 -0600

    Js para los submenus

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -29,4 +29,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
     
     btn_close.addEventListener("click", showHiddenMenu)
     
+    // Desplegar submenus
+    let menu__item = document.querySelectorAll(".menu-mobile__item")
+    menu__item.forEach(item => {
+        item.addEventListener("click", (event) => {
+            let submenu = item.lastElementChild
+            if(submenu.className == "menu-mobile__submenu"){
+                if(submenu.style.display === "block"){
+                    submenu.style.display ="none"
+                }else{
+                    submenu.style.display ="block"
+                }
+            }
+            
+        })
+    })
 })
